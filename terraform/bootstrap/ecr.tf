304d52383c74ba339498d0bb41d1720a980f5bae
SaiPisey2 <piseysai0202@gmail.com>
2026-07-09 01:59:23 +0530

    feat(tf): ECR repositories with scan-on-push and lifecycle

---
+++ terraform/bootstrap/ecr.tf
@@ -0,0 +1,29 @@
+resource "aws_ecr_repository" "service" {
+  for_each = toset(var.ecr_repositories)
+
+  name                 = "${var.project}/${each.value}"
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = false
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "service" {
+  for_each   = aws_ecr_repository.service
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 15 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 15
+      }
+      action = { type = "expire" }
+    }]
+  })
+}
